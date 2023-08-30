0a8a67d37bc08b5894c0a3d95648fbd2fe794188
rita7811 <92129512+rita7811@users.noreply.github.com>
2023-08-29 22:27:53 -0400

    initial commit

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<VillaDTO> GetVillas() {
+            return VillaStore.VillaList;
+        }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id) {
+            return VillaStore.VillaList.FirstOrDefault(x => x.Id == id);
+        }
+           
+	}
+}
+
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+	public static class VillaStore
+	{
+		public static List<VillaDTO> VillaList = new List<VillaDTO> {
+				new VillaDTO{Id=1,Name="Pool View"},
+				new VillaDTO{Id=2,Name="Beach View"}
+		};
+	}
+}
+
