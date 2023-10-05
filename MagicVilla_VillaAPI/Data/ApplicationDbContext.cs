5235846f26e9c0775b7ba39c3babea5bf181200b
rita7811 <92129512+rita7811@users.noreply.github.com>
2023-10-05 00:39:14 -0400

    Entity Framework Core: Database Models, DbSet Entity, Connection String, Create Villa Table

---
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MagicVilla_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaAPI.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option)
+			: base(option)
+		{
+		}
+
+		public DbSet<Villa> Villas { get; set; }
+	}
+}
+
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,7 +1,13 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using MagicVilla_VillaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApplicationDbContext>(option => {
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 //Content Negotiations
 builder.Services.AddControllers(option => {
     //option.ReturnHttpNotAcceptable = true;
