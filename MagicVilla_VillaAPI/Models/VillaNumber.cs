e464c0a0ab39ffa1bfc099cba96921d320e9e125
rita7811 <92129512+rita7811@users.noreply.github.com>
2023-10-20 19:47:57 -0400

    Villa Number Models

---
+++ MagicVilla_VillaAPI/Models/VillaNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla_VillaAPI.Models
+{
+	public class VillaNumber
+	{
+		[Key, DatabaseGenerated(DatabaseGeneratedOption.None)]  //provided by the user
+		public int VillaNo { get; set; }
+		public string SpecialDetails { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public DateTime UpdatedDate { get; set; }
+	}
+}
+
